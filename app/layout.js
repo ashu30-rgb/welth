8c332c64b09e604911c1bfaac70baf5b02eaf84b
Ashutosh <ashutosh728173@gmail.com>
2025-01-28 09:31:23 +0530

    Added login check to chat button

--- app/layout.js
+++ app/layout.js
@@ -1,13 +1,14 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
-import { ClerkProvider } from "@clerk/nextjs"; 
+import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "sonner";
 import ChatBot from "@/components/chat-bot";
+import { SignedIn } from "@clerk/nextjs";
 
 const inter = Inter({
-  subsets: ['latin','vietnamese'], // Specify the subset(s)
-  preload: false,      // Preloading enabled
+  subsets: ["latin", "vietnamese"], // Specify the subset(s)
+  preload: false, // Preloading enabled
 });
 
 export const metadata = {
@@ -21,16 +22,16 @@ export default function RootLayout({ children }) {
       <html lang="en">
         <body className={inter.className}>
           <Header />
-          <main className="min-h-screen">
-            {children}
-          </main>
-          <Toaster richColors/>
+          <main className="min-h-screen">{children}</main>
+          <Toaster richColors />
           <footer className="bg-blue-50 py-12">
             <div className="container mx-auto px-4 text-center text-gray-600">
               <p>Made with ❤️ by Ashutosh</p>
             </div>
           </footer>
-          <ChatBot/>
+          <SignedIn>
+            <ChatBot />
+          </SignedIn>
         </body>
       </html>
     </ClerkProvider>
